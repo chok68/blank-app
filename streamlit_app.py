cc0cd3684239f507aacaa43b73a2bb940d13bb2e
chok68 <jesusorozco@yahoo.com>
2025-01-07 07:16:00 +0000

    initial chat

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,24 @@
+import snowflake.snowpark
 import streamlit as st
-import pandas as pd
+import snowflake.connector
+from snowflake.snowpark.session import Session
+from snowflake.cortex import Complete
 
-st.title("🎈 Esta será nuestra app ? ? ")
-st.write(
-    "Panditas_2"
-)
+session = Session.builder.config("connection_name", "default").create()
+
+prompt: str = st.chat_input("Enter a prompt here")
+
+USER = "user"
+ASSISTANT = "assistant"
+
+if prompt:
+    st.chat_message(USER).write(prompt)
+
+    response = Complete("mistral-large", prompt, session=session)
+
+    st.chat_message(ASSISTANT).write(response)
+
+    # st.chat_message(ASSISTANT).write(f"You wrote {prompt}")
+
+# session = Session.builder.config("connection_name", "default").create()
+# print(Complete("mistral-large", "how do snowflakes get their unique patterns?"))
